abd28fc62fc81f7cc5675656212b6254ca1b9968
Tree&DB Hunter <40912698+SilverTree7622@users.noreply.github.com>
2020-07-31 20:46:41 +0900

    add some helpful comments

--- server.js
+++ server.js
@@ -3,47 +3,56 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io').listen(server);
 
+// using css / js / assets
 app.use('/css',express.static(__dirname + '/css'));
 app.use('/js',express.static(__dirname + '/js'));
 app.use('/assets',express.static(__dirname + '/assets'));
 
+// send index file
 app.get('/',function(req,res){
     res.sendFile(__dirname+'/index.html');
 });
 
+// declare last player id
 server.lastPlayderID = 0;
 
+// temp server connection
 server.listen(process.env.PORT || 8081,function(){
     console.log('Listening on '+server.address().port);
 });
 
+// when connected
 io.on('connection',function(socket){
-
+    // add new player to world
     socket.on('newplayer',function(){
+
         socket.player = {
             id: server.lastPlayderID++,
             x: randomInt(100,400),
             y: randomInt(100,400)
         };
-        socket.emit('allplayers',getAllPlayers());
-        socket.broadcast.emit('newplayer',socket.player);
+        
+        // function return player list in array
+        socket.emit('allplayers', getAllPlayers());
 
+        socket.broadcast.emit('newplayer', socket.player);
+
+        // when move player position
         socket.on('click',function(data){
             console.log('click to '+data.x+', '+data.y);
             socket.player.x = data.x;
             socket.player.y = data.y;
-            io.emit('move',socket.player);
+            io.emit('move', socket.player);
         });
 
+        // when player quit do remove work
         socket.on('disconnect',function(){
-            io.emit('remove',socket.player.id);
+            io.emit('remove', socket.player.id);
         });
     });
 
-    socket.on('test',function(){
-        console.log('test received');
-    });
 });
+
 
 function getAllPlayers(){
     var players = [];
